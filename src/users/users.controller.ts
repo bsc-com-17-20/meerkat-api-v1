0041c1dfdaf862e301201e59e396c55f575a5205
bsc-com-17-20 <bsc-com-17-20@unima.ac.mw>
2023-04-17 21:45:18 +0200

    added a create user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Get()
   getUsers() {}
 
   @Post()
-  createUser() {}
+  createUser(@Body() createUserDto: CreateUserDto) {
+    this.usersService.createUser(createUserDto);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './models/users.entity';
+import { Repository } from 'typeorm';
+import { CreateUserParams } from 'src/utils/types';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  fetchUsers() {}
+
+  createUser(userDetails: CreateUserParams) {
+    const newUser = this.userRepository.create({ ...userDetails });
+    return this.userRepository.save(newUser);
+  }
+}
