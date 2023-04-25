f4d0613db42763a234ebbdeb577a555f1c009f79
bsc-com-17-20 <bsc-com-17-20@unima.ac.mw>
2023-04-25 22:46:08 +0200

    removed unused app.service and controller files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { User } from './users/models/users.entity';
@@ -34,7 +32,5 @@ import { Reply } from './replies/models/replies.entity';
     AuthModule,
     RepliesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
