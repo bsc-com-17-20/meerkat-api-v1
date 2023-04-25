66a097a04142cd400c5f88c01a9632c1b58e2376
bsc-com-17-20 <bsc-com-17-20@unima.ac.mw>
2023-04-25 20:33:01 +0200

    fixed: entity metadata not found error

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,7 @@ import { PostsModule } from './posts/posts.module';
 import { Post } from './posts/models/posts.entity';
 import { AuthModule } from './auth/auth.module';
 import { RepliesModule } from './replies/replies.module';
+import { Reply } from './replies/models/replies.entity';
 
 @Module({
   imports: [
@@ -23,7 +24,7 @@ import { RepliesModule } from './replies/replies.module';
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
-        entities: [User, Board, Post],
+        entities: [User, Board, Post, Reply],
         synchronize: true,
       }),
     }),
