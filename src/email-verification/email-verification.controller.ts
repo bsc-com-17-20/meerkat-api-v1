1d8fb4e2babfbba31afa3fe75ed83681278828ed
bsc-com-17-20 <bsc-com-17-20@unima.ac.mw>
2023-05-18 11:03:19 +0200

    added swagger documentation for email-verfication service

--- src/email-verification/email-verification.controller.ts
+++ src/email-verification/email-verification.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { EmailVerificationService } from './email-verification.service';
 import { Public } from 'src/auth/decorators';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('email-verification')
 @Controller('email-verification')
 export class EmailVerificationController {
   constructor(
@@ -9,6 +11,15 @@ export class EmailVerificationController {
   ) {}
 
   @Public()
+  @ApiOperation({
+    summary: "Link sent to a user's email that activates a user's account",
+    description:
+      'link that upon clicking will send a get request with a confirmationCode param that sets a user account to active',
+    operationId: 'verifyUser',
+  })
+  @ApiResponse({ status: 200, description: 'Successful operation' })
+  @ApiResponse({ status: 401, description: 'Unauthorized operation' })
+  @ApiResponse({ status: 405, description: 'Invalid input' })
   @Get(':confimationCode')
   async verifyUser(@Param('confirmationCode') confirmationCode: string) {
     return await this.emailVerificationService.verifyUser(confirmationCode);
